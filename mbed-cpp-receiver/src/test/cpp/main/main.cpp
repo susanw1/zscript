d2b87cdcce45f790ca261add10ab3518edcc1e3d
Robert <robert.witts.rmw@gmail.com>
2020-12-23 17:03:28 +0000

    Reduced arm version to working edition

--- mbed-cpp-receiver/src/test/cpp/main/main.cpp
+++ mbed-cpp-receiver/src/test/cpp/main/main.cpp
@@ -33,16 +33,7 @@
 #include "commands/RCodeNotificationHostCommand.hpp"
 #include "commands/RCodeCapabilitiesCommand.hpp"
 #include "executionspace/RCodeExecutionSpaceChannel.hpp"
-//#include "../commands/RCodeIdentifyCommand.hpp"
-//#include "../commands/persistence/RCodeFetchGiudCommand.hpp"
-//#include "../commands/persistence/RCodePersistentFetchCommand.hpp"
-//#include "../commands/persistence/RCodePersistentStoreCommand.hpp"
-//#include "../commands/persistence/RCodeStoreGiudCommand.hpp"
-//#include "../commands/persistence/RCodeStoreMacAddressCommand.hpp"
-//#include "../commands/persistence/RCodeMbedFlashPersistence.hpp"
-//#include "../commands/I2C/RCodeI2cSubsystem.hpp"
-//#include "../commands/I2C/RCodeI2cSetupCommand.hpp"
-//#include "../commands/I2C/RCodeI2cSendCommand.hpp"
+#include "../commands/RCodeIdentifyCommand.hpp"
 
 #include "UipUdpCommandChannel.hpp"
 
@@ -52,7 +43,6 @@ int main(void) {
     UipEthernet uip(mac, PA_7, PA_6, PA_5, PA_4);
     while (uip.connect(5))
         ;
-//    I2cManager::init();
     RCode r(NULL, 0);
     UipUdpCommandChannel channel(&r, &uip, 4889);
     RCodeExecutionSpaceChannel execCh = RCodeExecutionSpaceChannel(&r, r.getSpace());
@@ -68,19 +58,9 @@ int main(void) {
     RCodeExecutionCommand cmd5 = RCodeExecutionCommand(r.getSpace());
     RCodeExecutionStoreCommand cmd6 = RCodeExecutionStoreCommand(r.getSpace());
     RCodeNotificationHostCommand cmd7 = RCodeNotificationHostCommand(&r);
-//    RCodeIdentifyCommand cmd8 = RCodeIdentifyCommand();
+    RCodeIdentifyCommand cmd8 = RCodeIdentifyCommand();
 
-//    RCodeMbedFlashPersistence persist = RCodeMbedFlashPersistence();
-//    RCodeFetchGiudCommand cmd9 = RCodeFetchGiudCommand(&persist);
-//    RCodePersistentFetchCommand cmd10 = RCodePersistentFetchCommand(&persist);
-//    RCodePersistentStoreCommand cmd11 = RCodePersistentStoreCommand(&persist);
-//    RCodeStoreGiudCommand cmd12 = RCodeStoreGiudCommand(&persist);
-//    RCodeStoreMacAddressCommand cmd13 = RCodeStoreMacAddressCommand(&persist);
-
-//    RCodeI2cSubsystem::init();
-//    RCodeI2cSetupCommand cmd14 = RCodeI2cSetupCommand();
-//    RCodeI2cSendCommand cmd15 = RCodeI2cSendCommand();
-//    r.getCommandFinder()->registerCommand(&cmd8);
+    r.getCommandFinder()->registerCommand(&cmd8);
     r.getCommandFinder()->registerCommand(&cmd0);
     r.getCommandFinder()->registerCommand(&cmd3);
     r.getCommandFinder()->registerCommand(&cmd1);
@@ -90,14 +70,6 @@ int main(void) {
     r.getCommandFinder()->registerCommand(&cmd5);
     r.getCommandFinder()->registerCommand(&cmd6);
 
-//    r.getCommandFinder()->registerCommand(&cmd9);
-//    r.getCommandFinder()->registerCommand(&cmd10);
-//    r.getCommandFinder()->registerCommand(&cmd11);
-//    r.getCommandFinder()->registerCommand(&cmd12);
-//    r.getCommandFinder()->registerCommand(&cmd13);
-
-//    r.getCommandFinder()->registerCommand(&cmd14);
-//    r.getCommandFinder()->registerCommand(&cmd15);
     while (true) {
         r.progressRCode();
         uip.tick();
