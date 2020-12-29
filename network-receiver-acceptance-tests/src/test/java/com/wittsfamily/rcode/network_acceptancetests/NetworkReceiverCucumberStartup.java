41447c16251a094cd5e3f5fce0e54647a18d8e90
susanw1 <susanwitts1@gmail.com>
2020-12-29 19:04:55 +0000

    Added java-receiver-acceptance-tests module and sysproperties for device ident

--- network-receiver-acceptance-tests/src/test/java/com/wittsfamily/rcode/network_acceptancetests/NetworkReceiverCucumberStartup.java
+++ network-receiver-acceptance-tests/src/test/java/com/wittsfamily/rcode/network_acceptancetests/NetworkReceiverCucumberStartup.java
@@ -8,12 +8,11 @@ import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Consumer;
 
 import javax.swing.JOptionPane;
 
+import org.assertj.core.api.Assertions;
 import org.junit.AssumptionViolatedException;
 
 import com.wittsfamily.rcode_acceptance_tests.RCodeAcceptanceTestConnectionManager;
@@ -30,67 +29,32 @@ public class NetworkReceiverCucumberStartup {
             hasStarted = true;
             try {
                 NetworkAcceptanceTestConnection connection = new NetworkAcceptanceTestConnection();
+                BroadcastListener listener = new BroadcastListener();
+                connection.registerBroadcastListener(listener);
+
+                connection.watchResponses();
+
                 SocketAddress target = null;
-                while (target == null) {
-                    boolean foundPort = false;
-                    int port = 0;
-                    while (!foundPort) {
-                        String portStr = JOptionPane.showInputDialog("Enter the target device's port");
-                        if (portStr == null) {
-                            throw new AssumptionViolatedException("No port entered");
-                        }
-                        try {
-                            port = Integer.parseInt(portStr);
-                            foundPort = true;
-                        } catch (NumberFormatException e) {
-                            throw new AssumptionViolatedException("Invalid port", e);
-                        }
-                    }
-                    connection.watchResponses();
-                    BroadcastListener listener = new BroadcastListener();
 
-                    connection.registerBroadcastListener(listener);
-                    while (target == null) {
-                        int lastSize = listener.getByteResults().size();
-                        connection.scanForTargets("*R0\n".getBytes(StandardCharsets.UTF_8), port);
-                        long startTime = System.currentTimeMillis();
+                do {
+                    Integer port = Integer.getInteger("rcode.network.acceptance-tests.port");
 
-                        while (listener.getByteResults().size() <= lastSize && System.currentTimeMillis() - startTime < 2500) {
-                            Thread.sleep(500);
-                        }
-                        if (listener.getByteResults().size() == lastSize) {
-                            int result = JOptionPane.showOptionDialog(null, "No devices have been found, do you want to re-send the broadcast, or choose a new port?",
-                                    "No devices found", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, new String[] { "Re-send", "Choose new port", "Give up" },
-                                    "Re-send");
-                            if (result == JOptionPane.YES_OPTION) {
-                                continue;
-                            } else if (result == JOptionPane.NO_OPTION) {
-                                break;
-                            } else {
-                                throw new AssumptionViolatedException("Search for target device aborted");
-                            }
-                        } else {
-                            String[] options = new String[listener.getByteResults().size()];
-                            for (int i = 0; i < listener.getByteResults().size(); i++) {
-                                options[i] = StandardCharsets.UTF_8.decode(ByteBuffer.wrap(listener.getByteResults().get(i))).toString();
-                            }
-                            String result = (String) JOptionPane.showInputDialog(null, "These responses have been received, please select the correct response",
-                                    "Select target device", JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
-                            if (result != null) {
-                                int targetResult = -1;
-                                for (int i = 0; i < options.length; i++) {
-                                    if (options[i].equals(result)) {
-                                        targetResult = i;
-                                        break;
-                                    }
-                                }
-                                target = listener.getAddresses().get(targetResult);
-                            }
-                        }
+                    if (port == null) {
+                        port = promptUserForPort();
                     }
-                }
-                connection.setTarget(target);
+
+                    String deviceIdent = System.getProperty("rcode.network.acceptance-tests.device-ident", null);
+
+                    if (deviceIdent == null) {
+                        target = promptUserForTarget(connection, listener, port);
+                    } else {
+                        target = checkForSpecifiedTarget(connection, listener, port, deviceIdent);
+                    }
+
+                } while (target == null);
+
                 connection.clearListeners();
+                connection.setTarget(target);
                 RCodeAcceptanceTestConnectionManager.registerConnection(connection);
             } catch (InterruptedException e) {
                 System.err.println("interrupted");
@@ -100,28 +64,106 @@ public class NetworkReceiverCucumberStartup {
         }
     }
 
+    /**
+     * @param connection the (UDP) connection to use to send probe messages
+     * @param listener   the registered listener to pick up broadcast responses
+     * @param port       the port to use
+     * @return the SocketAddress of identified device, or null if aborted
+     * @throws InterruptedException
+     * @throws AssumptionViolatedException if search is aborted
+     */
+    private SocketAddress promptUserForTarget(NetworkAcceptanceTestConnection connection, BroadcastListener listener, int port) throws InterruptedException {
+        while (true) {
+            doBroadcastScan(connection, listener, port);
+
+            if (listener.getResponseCount() == 0) {
+                int result = JOptionPane.showOptionDialog(null, "No devices have been found, do you want to re-send the broadcast, or choose a new port?",
+                        "No devices found", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, new String[] { "Re-send", "Choose new port", "Give up" },
+                        "Re-send");
+                if (result == JOptionPane.YES_OPTION) {
+                    continue;
+                } else if (result == JOptionPane.NO_OPTION) {
+                    return null;
+                } else {
+                    throw new AssumptionViolatedException("Search for target device aborted");
+                }
+            } else {
+                Object[] options = listener.getResponses().toArray();
+                String chosen = (String) JOptionPane.showInputDialog(null, "These responses have been received, please select the correct response",
+                        "Select target device", JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+
+                if (chosen != null) {
+                    for (int i = 0; i < options.length; i++) {
+                        if (options[i].equals(chosen)) {
+                            return listener.getAddresses().get(i);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private SocketAddress checkForSpecifiedTarget(NetworkAcceptanceTestConnection connection, BroadcastListener listener, int port, String deviceIdent)
+            throws InterruptedException {
+        doBroadcastScan(connection, listener, port);
+
+        int index = listener.getResponses().indexOf(deviceIdent);
+        if (index == -1) {
+            Assertions.fail("No device found matching '" + deviceIdent + "'; responses found were: " + listener.getResponses());
+        }
+        return listener.getAddresses().get(index);
+    }
+
+    private void doBroadcastScan(NetworkAcceptanceTestConnection connection, BroadcastListener listener, int port) throws InterruptedException {
+        int previousResponseCount = listener.getResponseCount();
+        connection.scanForTargets("*R0\n".getBytes(StandardCharsets.UTF_8), port);
+        long startTime = System.currentTimeMillis();
+        while (listener.getResponseCount() == previousResponseCount && System.currentTimeMillis() - startTime < 2500) {
+            Thread.sleep(500);
+        }
+    }
+
+    private int promptUserForPort() {
+        boolean foundPort = false;
+
+        int port = 0;
+        String initialPromptString = "Enter the target device's port";
+        String promptString = initialPromptString;
+
+        while (!foundPort) {
+            String portString = JOptionPane.showInputDialog(promptString);
+            if (portString == null) {
+                throw new AssumptionViolatedException("No port entered");
+            }
+            try {
+                port = Integer.parseInt(portString);
+                foundPort = true;
+            } catch (NumberFormatException e) {
+                promptString = e.toString() + "\n" + initialPromptString;
+            }
+        }
+        return port;
+    }
+
     private static class BroadcastListener implements Consumer<DatagramPacket> {
-        private final Lock lock = new ReentrantLock();
-        private List<byte[]> byteResults = new ArrayList<>();
+        private List<String> responses = new ArrayList<>();
         private List<SocketAddress> addresses = new ArrayList<>();
 
         @Override
-        public void accept(DatagramPacket p) {
-            lock.lock();
-            byteResults.add(p.getData());
+        public synchronized void accept(DatagramPacket p) {
+            responses.add(StandardCharsets.UTF_8.decode(ByteBuffer.wrap(p.getData())).toString());
             addresses.add(p.getSocketAddress());
-            lock.unlock();
         }
 
-        public List<byte[]> getByteResults() {
-            lock.lock();
-            lock.unlock();
-            return new ArrayList<>(byteResults);
+        public synchronized int getResponseCount() {
+            return responses.size();
         }
 
-        public List<SocketAddress> getAddresses() {
-            lock.lock();
-            lock.unlock();
+        public synchronized List<String> getResponses() {
+            return new ArrayList<>(responses);
+        }
+
+        public synchronized List<SocketAddress> getAddresses() {
             return new ArrayList<>(addresses);
         }
 
