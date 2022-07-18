5b162cf09abcae3d9394ac0bba07361b74800f42
Robert <robert.witts.rmw@gmail.com>
2022-07-18 17:48:55 +0100

    Removed old bigfield based capabilities system

--- receivers/native/generic-core-2/src/main/c++/zcode/modules/core/ZcodeCapabilitiesCommand.hpp
+++ receivers/native/generic-core-2/src/main/c++/zcode/modules/core/ZcodeCapabilitiesCommand.hpp
@@ -33,11 +33,6 @@ public:
         out->writeField16('C', MODULE_CAPABILITIES(000));
         out->writeField16('M', COMMAND_SWITCH_EXISTS_BOTTOM_BYTE);
         out->writeField32('N', (uint32_t) ZP::fieldNum);
-        uint8_t moduleName[slot.getZcode()->getModuleNumber()];
-        for (uint8_t i = 0; i < slot.getZcode()->getModuleNumber(); ++i) {
-            moduleName[i] = (uint8_t) slot.getZcode()->getModules()[i]->moduleId;
-        }
-        out->writeBigHexField((uint8_t*) moduleName, slot.getZcode()->getModuleNumber());
     }
 
 };
