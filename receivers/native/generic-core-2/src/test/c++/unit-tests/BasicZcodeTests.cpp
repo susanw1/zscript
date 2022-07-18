7dbf5549887f2cfd594aee5ec12d08b46525953a
Robert <robert.witts.rmw@gmail.com>
2022-07-18 17:51:52 +0100

    Updated tests to cope with changed capabilities

--- receivers/native/generic-core-2/src/test/c++/unit-tests/BasicZcodeTests.cpp
+++ receivers/native/generic-core-2/src/test/c++/unit-tests/BasicZcodeTests.cpp
@@ -77,15 +77,15 @@ int main(void) {
         std::cerr << "Failed on Consecutive Echo with errors\n";
         return 1;
     }
-    if (!ZcodeTestingSystem::tryTest("Z0", "SC3107M7N14+000102\n")) {
+    if (!ZcodeTestingSystem::tryTest("Z0", "SC3107M7N14\n")) {
         std::cerr << "Failed on Capabilities\n";
         return 1;
     }
-    if (!ZcodeTestingSystem::tryTest("Z0000", "SC3107M7N14+000102\n")) {
+    if (!ZcodeTestingSystem::tryTest("Z0000", "SC3107M7N14\n")) {
         std::cerr << "Failed on Capabilities\n";
         return 1;
     }
-    if (!ZcodeTestingSystem::tryTest("Z", "SC3107M7N14+000102\n")) {
+    if (!ZcodeTestingSystem::tryTest("Z", "SC3107M7N14\n")) {
         std::cerr << "Failed on Capabilities with no 0s\n";
         return 1;
     }
