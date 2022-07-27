2f43d056a28f7f3101478e5a349765ef06991d1b
Robert <robert.witts.rmw@gmail.com>
2022-07-27 16:11:24 +0100

    [#27] Fixed clock system

--- receivers/native/arm/no-os-F030/src/main/c++/arm-no-os/system/clock/specific/ClockManagercpp.hpp
+++ receivers/native/arm/no-os-F030/src/main/c++/arm-no-os/system/clock/specific/ClockManagercpp.hpp
@@ -14,17 +14,13 @@ template<class LL>
 Clock<LL> ClockManager<LL>::clocks[LL::systemClockCount] = {
         Clock<LL>(16000, SysClock, HSI),
         Clock<LL>(16000, HSI, NONE),
-        Clock<LL>(0, HSI48, NONE),
+        Clock<LL>(0, HSI14, NONE),
         Clock<LL>(0, LSI, NONE),
         Clock<LL>(0, HSE, NONE),
         Clock<LL>(0, LSE, NONE),
-        Clock<LL>(0, VCO, NONE),
-        Clock<LL>(0, PLL_P, VCO),
-        Clock<LL>(0, PLL_Q, VCO),
-        Clock<LL>(0, PLL_R, VCO),
+        Clock<LL>(0, PLL, HSI),
         Clock<LL>(16000, HCLK, SysClock),
-        Clock<LL>(16000, PCLK_1, HCLK),
-        Clock<LL>(16000, PCLK_2, HCLK)
+        Clock<LL>(16000, PCLK, HCLK),
 };
 
 #endif /* SRC_MAIN_C___LOWLEVEL_CLOCKSLOWLEVEL_SPECIFIC_CLOCKMANAGERCPP_HPP_ */
