85c4802547b2e6929a5fd08b19d6ccf2ab43d353
Robert <robert.witts.rmw@gmail.com>
2022-12-19 01:34:02 +0000

    #45 Added first attempt at auto-generated header files for devices

---
+++ receivers/native/arm/implementations/no-os-stm32/README.txt
@@ -0,0 +1,3 @@
+PLEASE NOTE FOR licensing:
+The below files belong to STMicro, or are derived from them (except for the python), and are licensed according to their terms.
+Please be careful.
---
+++ receivers/native/arm/implementations/no-os-stm32/src/main/c++/stm32/stm32Setup.h
@@ -0,0 +1,44 @@
+#ifndef DEVICE_DEFINITION_FILE_H
+#define DEVICE_DEFINITION_FILE_H
+
+#include "switchingFile.h"
+
+#define Pin0 0
+#define Pin1 1
+#define Pin2 2
+#define Pin3 3
+#define Pin4 4
+#define Pin5 5
+#define Pin6 6
+#define Pin7 7
+#define Pin8 8
+#define Pin9 9
+#define Pin10 10
+#define Pin11 11
+#define Pin12 12
+#define Pin13 13
+#define Pin14 14
+#define Pin15 15
+
+#define GPIO_AF0 0
+#define GPIO_AF1 1
+#define GPIO_AF2 2
+#define GPIO_AF3 3
+#define GPIO_AF4 4
+#define GPIO_AF5 5
+#define GPIO_AF6 6
+#define GPIO_AF7 7
+#define GPIO_AF8 8
+#define GPIO_AF9 9
+#define GPIO_AF10 10
+#define GPIO_AF11 11
+#define GPIO_AF12 12
+#define GPIO_AF13 13
+#define GPIO_AF14 14
+#define GPIO_AF15 15
+
+#ifndef ZCODE_IDENTIFY_PLATFORM_HARDWARE_VERSION
+#define ZCODE_IDENTIFY_PLATFORM_HARDWARE_VERSION 0x0001
+#endif
+
+#endif /* DEVICE_DEFINITION_FILE_H */
