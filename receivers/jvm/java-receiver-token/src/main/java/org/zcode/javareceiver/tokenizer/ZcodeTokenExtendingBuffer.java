35d8b92b5deda8e6b36b77559fc09058829505fa
Alicia <alicia.spelt.properly@gmail.com>
2023-07-10 23:33:49 +0100

    [#58] Split token ring buffer responsibilities and implemented extending buffer

---
+++ receivers/jvm/java-receiver-token/src/main/java/org/zcode/javareceiver/tokenizer/ZcodeTokenExtendingBuffer.java
@@ -0,0 +1,29 @@
+package org.zcode.javareceiver.tokenizer;
+
+import static java.text.MessageFormat.format;
+
+import java.util.Arrays;
+
+public class ZcodeTokenExtendingBuffer extends ZcodeTokenArrayBuffer {
+    public ZcodeTokenExtendingBuffer() {
+        super(20);
+    }
+
+    @Override
+    protected boolean checkAvailableCapacityFrom(int writeCursor, int size) {
+        return true;
+    }
+
+    @Override
+    int offset(int index, int offset) {
+        if (index < 0 || offset < 0) {
+            throw new IllegalArgumentException(format("Unexpected values [pos={}, offset={}]", index, offset));
+        }
+        if (index + offset >= data.length) {
+            int newSize = index + offset + data.length;
+            data = Arrays.copyOf(data, newSize);
+        }
+        return index + offset;
+    }
+
+}
