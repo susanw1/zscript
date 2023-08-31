c04e414eded595225c66c49aba2366be857ad504
susanw1 <susanwitts1@gmail.com>
2023-08-31 16:00:30 +0100

    [#104] Demo projects, first cut

---
+++ demo/demo-01/client/src/main/java/net/zscript/demo01/client/Main.java
@@ -0,0 +1,11 @@
+package net.zscript.demo01.client;
+
+import net.zscript.model.modules.base.CoreModule;
+
+public class Main {
+    public static void main(String[] args) {
+        CoreModule.activate()
+                .build();
+        
+    }
+}
