cfeb2d8bbde082c16e62b300c8179267aac8f8a5
Alicia <alicia.spelt.properly@gmail.com>
2023-11-10 01:12:28 +0000

    [#139] Added nice cli to morse demo

--- demo/demo-morse/client/src/main/java/net/zscript/demo/morse/MorseReceiver.java
+++ demo/demo-morse/client/src/main/java/net/zscript/demo/morse/MorseReceiver.java
@@ -4,6 +4,9 @@ import static net.zscript.model.modules.base.PinsModule.DigitalNotificationSecti
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import net.zscript.javaclient.commandbuilder.commandnodes.ResponseCaptor;
 import net.zscript.javaclient.devices.Device;
@@ -14,6 +17,8 @@ import net.zscript.model.modules.base.PinsModule.DigitalSetupCommand.Builder.Mod
 import net.zscript.model.modules.base.PinsModule.DigitalSetupCommand.Builder.NotificationMode;
 
 public class MorseReceiver {
+    private final ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+
     private final MorseTranslator translator;
     private final Device          device;
     private final long            ditPeriodMaxUs;
@@ -21,7 +26,9 @@ public class MorseReceiver {
 
     private final List<MorseElement> currentCharacter = new ArrayList<>();
 
-    private long lastTimeNano;
+    private long    lastTimeNano;
+    private boolean isHigh       = false;
+    private boolean hasWrittenWS = true;
 
     public MorseReceiver(MorseTranslator translator, Device device, long ditPeriodMaxUs, int pin) {
         this.translator = translator;
@@ -41,31 +48,61 @@ public class MorseReceiver {
                 long   current = System.nanoTime();
                 double time    = (current - lastTimeNano) / 1000.0 / ditPeriodMaxUs;
                 if (content.getValue() != Value.High) {
+                    hasWrittenWS = false;
+                    isHigh = false;
                     if (time > 2) {
                         currentCharacter.add(MorseElement.DAR);
                     } else {
                         currentCharacter.add(MorseElement.DIT);
                     }
                 } else {
+                    isHigh = true;
                     if (time > 2) {
-                        System.out.print(translator.translate(currentCharacter));
-                        currentCharacter.clear();
-                        if (time > 6) {
-                            System.out.print(' ');
+                        if (!currentCharacter.isEmpty()) {
+                            System.out.print(translator.translate(currentCharacter));
+                            currentCharacter.clear();
                         }
+                        if (!hasWrittenWS) {
+                            if (time > 10) {
+                                System.out.print('\n');
+                            } else if (time > 6) {
+                                System.out.print(' ');
+                            }
+                        }
+                        hasWrittenWS = true;
                     }
                 }
                 lastTimeNano = current;
             });
-            device.sendAndWaitExpectSuccess(CoreModule.activateBuilder().build());
             device.sendAndWaitExpectSuccess(OuterCoreModule.channelSetupBuilder().setAssignNotification().build());
             device.sendAndWaitExpectSuccess(PinsModule.digitalSetupBuilder().setPin(pin).setMode(Mode.Input).setNotificationMode(NotificationMode.OnChange).build());
+            exec.scheduleAtFixedRate(this::checkReceive, 100000, ditPeriodMaxUs * 4, TimeUnit.MICROSECONDS);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
 
+    private void checkReceive() {
+
+        double time = (System.nanoTime() - lastTimeNano) / 1000.0 / ditPeriodMaxUs;
+        if (!isHigh && time > 4) {
+            emptyBuffer();
+            if (!hasWrittenWS && time > 10) {
+                System.out.print('\n');
+                hasWrittenWS = true;
+            }
+        }
+    }
+
+    private void emptyBuffer() {
+        if (!currentCharacter.isEmpty()) {
+            System.out.print(translator.translate(currentCharacter));
+            currentCharacter.clear();
+        }
+    }
+
     public void close() {
-        System.out.print(translator.translate(currentCharacter));
+        emptyBuffer();
+        exec.shutdown();
     }
 }
