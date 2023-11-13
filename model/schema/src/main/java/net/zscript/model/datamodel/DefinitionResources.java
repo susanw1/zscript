9f9cac5c689c0e7b2194d093c5a9f3df7fdbc0a0
susanw1 <susanwitts1@gmail.com>
2023-11-13 15:37:35 +0000

    [#110] Ensure module ID is checked properly

--- model/schema/src/main/java/net/zscript/model/datamodel/DefinitionResources.java
+++ model/schema/src/main/java/net/zscript/model/datamodel/DefinitionResources.java
@@ -12,7 +12,7 @@ public interface DefinitionResources {
         /** The name of this Module Bank. */
         String getName();
 
-        /** The module bank ID of this Module Bank (defines upper byte of the (2-byte) command id). */
+        /** The module bank ID of this Module Bank (defines upper byte of the (2-byte) command id). Value 0x00-0xff */
         int getId();
 
         /** List of words making up the package-name for this module bank. */
