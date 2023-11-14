ac8e359a896eaf3b36a58dea818eb6b76b3ee615
susanw1 <susanwitts1@gmail.com>
2023-11-14 19:00:48 +0000

    [#110] Add channel setup bit settings

--- receivers/native/arduino/src/main/c++/arduino/uart-module/commands/UartChannelSetup.hpp
+++ receivers/native/arduino/src/main/c++/arduino/uart-module/commands/UartChannelSetup.hpp
@@ -19,34 +19,48 @@ template<class ZP>
 class UartChannel;
 
 template<class ZP>
+class ZscriptUartOutStream;
+
+template<class ZP>
 class UartModule;
 
 namespace uart_module {
 
 template<class ZP>
-class ZscriptUartChannelSetupCommand: public ChannelSetup_CommandDefs {
+class ZscriptUartChannelSetupCommand : public ChannelSetup_CommandDefs {
 public:
     static void execute(ZscriptCommandContext<ZP> ctx) {
         uint16_t channelIndex;
         if (!ctx.getFieldCheckLimit(ReqChannel__C, Zscript<ZP>::zscript.getChannelCount(), ctx.getChannelIndex(), &channelIndex)) {
             return;
         }
-        ZscriptChannel<ZP> *selectedChannel = Zscript<ZP>::zscript.getChannels()[channelIndex];
+        UartChannel<ZP> *selectedChannel = Zscript<ZP>::zscript.getChannels()[channelIndex];
         if (selectedChannel->getAssociatedModule() != MODULE_FULL_ID) {
             ctx.status(ResponseStatus::VALUE_OUT_OF_RANGE);
             return;
         }
 
-        if (ctx.hasField(ReqFrequencySelection__F)) {
+        ZscriptUartOutStream<ZP> *channelOut = selectedChannel->getUartOutStream();
 
+        {
+            uint16_t freqIndex;
+            if (ctx.getField(ReqFrequencySelection__F, &freqIndex)) {
+                constexpr uint8_t freqCount = sizeof(ZP::uartSupportedFreqs) / sizeof(ZP::uartSupportedFreqs[0]);
+                if (freqIndex >= freqCount) {
+                    ctx.status(ResponseStatus::VALUE_OUT_OF_RANGE);
+                    return;
+                }
+                channelOut->setFrequencyIndex(freqIndex);
+            }
         }
-        constexpr uint8_t freqCount = sizeof(ZP::uartSupportedFreqs) / sizeof(ZP::uartSupportedFreqs[0]);
-        uint16_t freqIndex;
-        if (!ctx.getFieldCheckLimit(ReqFrequencySelection__F, freqCount, freqCount - 1, &freqIndex)) {
-            return;
+        {
+            uint16_t options;
+            if (ctx.getField(ReqOptions__B, &options)) {
+                channelOut->setConfigParityMode((options & (uint8_t) ReqOptions_Values::parityOn_field), (options & (uint8_t) ReqOptions_Values::parityOdd_field));
+                channelOut->setConfigStopBits(options & (uint8_t) ReqOptions_Values::doubleStop_field);
+            }
         }
-
-
+        UartUtil<ZP>::writeFrequencySelection(ctx.getOutStream(), channelOut->getFrequencyIndex());
     }
 
 };
