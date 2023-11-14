2b4f362aa60622d8b571276bab058b486bfa2f51
susanw1 <susanwitts1@gmail.com>
2023-11-14 19:00:48 +0000

    [#110] Add channel setup bit settings

--- receivers/native/arduino/src/main/c++/arduino/uart-module/channels/UartChannel.hpp
+++ receivers/native/arduino/src/main/c++/arduino/uart-module/channels/UartChannel.hpp
@@ -9,6 +9,7 @@
 #define SRC_MAIN_CPP_ARDUINO_SERIAL_MODULE_CHANNELS_ZSCRIPTSERIALCHANNEL_HPP_
 
 #include <ZscriptChannelBuilder.hpp>
+#include <net/zscript/model/modules/base/UartModule.hpp>
 #include "../../arduino-core-module/persistence/PersistenceSystem.hpp"
 
 namespace Zscript {
@@ -16,10 +17,19 @@ namespace Zscript {
 template<class ZP>
 class ZscriptUartOutStream : public AbstractOutStream<ZP> {
 private:
-    bool openB = false;
+    static constexpr uint8_t MODE_DATABITS = 0x06;
+    static constexpr uint8_t MODE_STOPBITS = 0x08;
+    static constexpr uint8_t MODE_PARITY = 0x30;
+
+    // index into the ZP::uartSupportedFreqs array
+    uint8_t currentFrequencyIndex;
+    // represents Arduino config: 00pp_sdd0 (pp=N,E,O parity; s=2 stop bits; dd=5,6,7,8 bits)
+    uint8_t currentSerialMode;
+    bool configToChange: 1;
+    bool openB: 1;
 
 public:
-    ZscriptUartOutStream() {
+    ZscriptUartOutStream() : currentFrequencyIndex(0), currentSerialMode(SERIAL_8N1), configToChange(false), openB(false) {
     }
 
     void open(uint8_t source) {
@@ -32,7 +42,16 @@ public:
     }
 
     void close() {
+        if (configToChange) {
+            configure();
+        }
         openB = false;
+    }
+
+    void configure() {
+//        Serial.flush();
+        Serial.begin(ZP::uartSupportedFreqs[currentFrequencyIndex], currentSerialMode);
+        configToChange = false;
     }
 
     void writeBytes(const uint8_t *bytes, uint16_t count, bool hexMode) {
@@ -48,6 +67,38 @@ public:
         }
     }
 
+    uint8_t getFrequencyIndex() {
+        return currentFrequencyIndex;
+    }
+
+    void setFrequencyIndex(uint8_t newIndex) {
+        currentFrequencyIndex = newIndex;
+        configToChange = true;
+    }
+
+    bool getConfigStopBits() {
+        return (currentSerialMode & MODE_STOPBITS) != 0;
+    }
+
+    void setConfigStopBits(bool twoBits) {
+        currentSerialMode = (currentSerialMode & ~MODE_STOPBITS) | (twoBits ? MODE_STOPBITS : 0);
+        configToChange = true;
+    }
+
+    /** 0 => 5 bits, 1 => 6 bits, 2 => 7 bits, 3 => 8 bits */
+    uint8_t getConfigDataBits() {
+        return (currentSerialMode & MODE_DATABITS) >> 1;
+    }
+
+    /** 0 => parity disabled, 2 => Odd parity, 3 => Even parity */
+    uint8_t getConfigParity() {
+        return (currentSerialMode & MODE_PARITY) >> 4;
+    }
+
+    void setConfigParityMode(bool enabled, bool oddParity) {
+        currentSerialMode = (currentSerialMode & ~MODE_PARITY) | (enabled ? 0x20 : 0) | (oddParity ? 0x10 : 0);
+        configToChange = true;
+    }
 };
 
 template<class ZP>
@@ -58,9 +109,7 @@ class UartChannel : public ZscriptChannel<ZP> {
 
     uint8_t buffer[ZP::uartChannelBufferSize];
     uint8_t tmp = 0;
-    bool usingTmp = false;
-    uint8_t currentFrequencyIndex;
-    uint8_t currentSerialMode;
+    bool usingTmp: 1;
 
 public:
     static uint8_t getNotifChannelPersistMaxLength() {
@@ -68,37 +117,21 @@ public:
     }
 
     UartChannel() :
-            ZscriptChannel<ZP>(&out, &tBuffer, 0x7, true), tBuffer(buffer, ZP::uartChannelBufferSize), tokenizer(tBuffer.getWriter(), 2) {
+            buffer(), ZscriptChannel<ZP>(&out, &tBuffer, uart_module::MODULE_FULL_ID, true),
+            tBuffer(buffer, ZP::uartChannelBufferSize), tokenizer(tBuffer.getWriter(), 2), usingTmp(false) {
     }
 
     void setup() {
-        Serial.begin(ZP::uartSupportedFreqs[0]);
+        out.configure();
     }
-
-    uint8_t getCurrentFrequencyIndex() {
-        return currentFrequencyIndex;
-    }
-
 
     bool setupStartupNotificationChannel() {
         return true;
     }
 
-//    void channelInfo(ZscriptCommandContext<ZP> ctx) {
-//        CommandOutStream<ZP> ctxOut = ctx.getOutStream();
-//        // RespChannelCount__N
-//        ctxOut.writeField('N', 0);
-//        // ??
-//        ctxOut.writeField('M', 0x7);
-//        ctxOut.writeField('I', 0);
-//    }
-//
-//    void channelSetup(ZscriptCommandContext<ZP> ctx) {
-//        if (ctx.hasField('P')) {
-//            uint8_t index = this->parser.getChannelIndex();
-//            PersistenceSystem<ZP>::writeSection(PersistenceSystem<ZP>::getNotifChannelIdOffset(), 1, &index);
-//        }
-//    }
+    ZscriptUartOutStream<ZP> *getUartOutStream() {
+        return &out;
+    }
 
     void moveAlong() {
         while (usingTmp || Serial.available() > 0) {
@@ -111,6 +144,8 @@ public:
             }
         }
     }
+
+
 };
 }
 #endif /* SRC_MAIN_CPP_ARDUINO_SERIAL_MODULE_CHANNELS_ZSCRIPTSERIALCHANNEL_HPP_ */
