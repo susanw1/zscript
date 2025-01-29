26b284183b6e0eefcd64ceb9d084daa77e5f9dc3
susanw1 <susanwitts1@gmail.com>
2025-01-29 11:16:50 +0000

    [#174] EchoAssignerTest

---
+++ clients/java-client-lib/client-main/src/test/java/net/zscript/javaclient/devicenodes/ConnectionBufferTest.java
@@ -0,0 +1,17 @@
+package net.zscript.javaclient.devicenodes;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ConnectionBufferTest {
+    @Test
+    public void should() {
+
+        Connection       connection = null;
+        EchoAssigner     b          = null;
+        int              c          = 0;
+        ConnectionBuffer buffer     = new ConnectionBuffer(connection, b, c);
+
+        Assertions.assertThat(0).isEqualTo(0);
+    }
+}
