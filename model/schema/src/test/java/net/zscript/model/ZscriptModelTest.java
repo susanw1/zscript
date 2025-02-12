526a5f9bdff4f1c37fd5a19fe2eae4ff74e61155
susanw1 <susanwitts1@gmail.com>
2025-02-12 00:42:45 +0000

    [#174] Model tests

--- model/schema/src/test/java/net/zscript/model/ZscriptModelTest.java
+++ model/schema/src/test/java/net/zscript/model/ZscriptModelTest.java
@@ -3,16 +3,19 @@ package net.zscript.model;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.Test;
 
+import net.zscript.model.datamodel.IntrinsicsDataModel.StatusModel;
 import net.zscript.model.datamodel.ZscriptDataModel.CommandModel;
+import net.zscript.model.datamodel.ZscriptDataModel.ModelComponent;
 import net.zscript.model.datamodel.ZscriptDataModel.ModuleModel;
 
 class ZscriptModelTest {
     @Test
     void shouldLoadRaw() {
-        ZscriptModel model = ZscriptModel.rawModel();
+        final ZscriptModel model = ZscriptModel.rawModel();
         assertThat(model.getIntrinsics().getStatus()).hasSize(33);
         assertThat(model.getIntrinsics().getRequestFields()).hasSize(1);
         assertThat(model.getIntrinsics().getResponseFields()).hasSize(1);
@@ -20,32 +23,61 @@ class ZscriptModelTest {
 
     @Test
     void shouldLoadDefinition() {
-        ZscriptModel model = ZscriptModel.standardModel();
+        final ZscriptModel model = ZscriptModel.standardModel();
         assertThat(model.getModuleBank("Base")).isPresent();
         assertThat(model.getModuleBank("Banana")).isNotPresent();
+
         Optional<ModuleModel> module = model.getModuleBank("Base").orElseThrow().getModule("Testing");
         assertThat(module).isPresent();
+
+        assertThat(model.getModule("Base", "Testing")).isPresent();
+        assertThat(model.getModule("Nonexistent", "Testing")).isNotPresent();
+        assertThat(model.getModule("Base", "Nonexistent")).isNotPresent();
 
         assertThat(module.get().getNotifications().get(2).getSections()).hasSize(2);
     }
 
     @Test
     void shouldFindCommands() {
-        ZscriptModel model = ZscriptModel.standardModel();
+        final ZscriptModel model = ZscriptModel.standardModel();
         assertThat(model.getCommand(0x0000)).isPresent();
         assertThat(model.getCommand(0x7b9d)).isPresent();
+        assertThat(model.getCommand(0x1234)).isNotPresent();
+        assertThatIllegalArgumentException().isThrownBy(() -> model.getCommand(0x10000))
+                .withMessageContaining("valid 16 bit unsigned ints");
+        assertThatIllegalArgumentException().isThrownBy(() -> model.getCommand(-1))
+                .withMessageContaining("valid 16 bit unsigned ints");
     }
 
     @Test
     void shouldFindNotifications() {
-        ZscriptModel model = ZscriptModel.standardModel();
+        final ZscriptModel model = ZscriptModel.standardModel();
         assertThat(model.getNotification(0x0001)).isPresent();
         assertThat(model.getNotification(0x7b94)).isPresent();
+
+        assertThat(model.getNotification(0x1234)).isNotPresent();
+        assertThatIllegalArgumentException().isThrownBy(() -> model.getNotification(0x10000))
+                .withMessageContaining("valid 16 bit unsigned ints");
+        assertThatIllegalArgumentException().isThrownBy(() -> model.getNotification(-1))
+                .withMessageContaining("valid 16 bit unsigned ints");
+    }
+
+    @Test
+    public void shouldGetStatus() {
+        final ZscriptModel model = ZscriptModel.rawModel();
+
+        assertThat(model.getStatus(0)).isPresent().get()
+                .extracting(StatusModel::getId, ModelComponent::getName)
+                .containsExactly(0, "success");
+        assertThat(model.getStatus(0x10)).isPresent().get()
+                .extracting(StatusModel::getId, ModelComponent::getName)
+                .containsExactly(0x10, "bufferOvrError");
+        assertThat(model.getStatus(0xf)).isNotPresent();
     }
 
     @Test
     void shouldIdentifyFieldTypeDefinition() {
-        ZscriptModel                 model   = ZscriptModel.standardModel();
+        final ZscriptModel           model   = ZscriptModel.standardModel();
         final Optional<CommandModel> command = model.getCommand(0x0001);
         assertThat(command).isPresent();
 
--- model/schema/src/test/java/net/zscript/model/datamodel/IntrinsicsTest.java
+++ model/schema/src/test/java/net/zscript/model/datamodel/IntrinsicsTest.java
@@ -1,28 +1,53 @@
 package net.zscript.model.datamodel;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
 import static java.util.Objects.requireNonNull;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.IOException;
-import java.io.InputStream;
-
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.fasterxml.jackson.databind.json.JsonMapper;
-
+import net.zscript.model.datamodel.IntrinsicsDataModel.Intrinsics;
+import net.zscript.model.datamodel.IntrinsicsDataModel.ZcharDefs;
 import net.zscript.model.loader.ModelLoader;
 
 public class IntrinsicsTest {
-    private final JsonMapper jsonMapper = ModelLoader.createJsonMapper();
+    private final JsonMapper          jsonMapper = ModelLoader.createJsonMapper();
+    private       IntrinsicsDataModel dataModel;
+
+    @BeforeEach
+    public void setup() throws IOException {
+        final InputStream resourceStream = requireNonNull(getClass().getResourceAsStream("/zscript-datamodel/intrinsics.yaml"), "resourceStream is null");
+        dataModel = jsonMapper.readValue(resourceStream, IntrinsicsDataModel.class);
+    }
 
     @Test
-    public void shouldLoadIntrinsics() throws IOException {
-        final InputStream   resourceStream = requireNonNull(getClass().getResourceAsStream("/zscript-datamodel/intrinsics.yaml"), "resourceStream is null");
-        IntrinsicsDataModel dataModel      = jsonMapper.readValue(resourceStream, IntrinsicsDataModel.class);
+    public void shouldLoadIntrinsics() {
+        final Intrinsics intrinsics = dataModel.getIntrinsics();
+        assertThat(intrinsics.getRequestFields()).hasSize(1);
+        assertThat(intrinsics.getResponseFields()).hasSize(1);
+        assertThat(intrinsics.getZchars()).hasSize(21);
+        assertThat(intrinsics.getStatus()).hasSize(33);
+    }
 
-        assertThat(dataModel.getIntrinsics().getRequestFields()).hasSize(1);
-        assertThat(dataModel.getIntrinsics().getResponseFields()).hasSize(1);
-        assertThat(dataModel.getIntrinsics().getZchars()).hasSize(21);
-        assertThat(dataModel.getIntrinsics().getStatus()).hasSize(33);
+    @Test
+    public void shouldEscapeChars() {
+        final Intrinsics intrinsics = dataModel.getIntrinsics();
+        assertThat(intrinsics.getZchars()).isNotEmpty();
+        final List<ZcharDefs> zchs = intrinsics.getZchars();
+
+        Map<Character, Character> escapes = Map.of('\n', 'n', '\0', '0', '\t', 't', '\r', 'r');
+        for (ZcharDefs zcd : zchs) {
+            if (escapes.containsKey(zcd.getCh())) {
+                assertThat(zcd.getChEscapedAsC()).isEqualTo("\\" + escapes.get(zcd.getCh()));
+            } else {
+                assertThat(zcd.getChEscapedAsC()).isEqualTo("" + zcd.getCh());
+            }
+        }
     }
 }
