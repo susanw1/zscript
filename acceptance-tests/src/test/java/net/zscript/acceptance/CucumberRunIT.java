e369d179e35e4c129c0b6db03b99e7bfd76dc504
susanw1 <susanwitts1@gmail.com>
2026-08-29 17:14:38 +0100

    [#168] Bumped versions: Cucumber

--- acceptance-tests/src/test/java/net/zscript/acceptance/CucumberRunIT.java
+++ acceptance-tests/src/test/java/net/zscript/acceptance/CucumberRunIT.java
@@ -4,13 +4,12 @@ import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
-@SelectClasspathResource("experiment")
+@SelectPackages("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "net.zscript.acceptance, net.zscript.experiment")
 public class CucumberRunIT {
 }
